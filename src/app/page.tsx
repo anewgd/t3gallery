4a24ff1bdf86b7c5d98972aad49c5cbe61a67454
anewgd <ephremabraham8997@gmail.com>
2024-12-26 09:50:14 +0300

    connected database through drizzle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockURLs = [
   "https://utfs.io/f/zyRHyeDUR8B00PHs3YzLwWaEsezl47o8M5IUk0hQySJRdnA6",
@@ -12,10 +13,16 @@ const mockImages = mockURLs.map((url, index) => ({
   id: index,
   url,
 }))
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main>
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (<div key={post.id}>{post.name}</div>))}
          {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} alt="Image" />
