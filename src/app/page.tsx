b6f24a3c8d9d320c67bf01a01eb579606b2e905b
anewgd <ephremabraham8997@gmail.com>
2025-01-03 15:21:17 +0300

    separated queries into separate file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,13 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
+import { getMyImages } from "~/server/queries";
 
 //Forces the page to be reloaded if a database change is detected
 export const dynamic = "force-dynamic";
 
 async function Images() {
-  const images = await db.query.images.findMany({
-    // order the images by id in descending order
-    orderBy: (model, {desc}) => desc(model.id),
-  });
+  const images = await getMyImages();
   return (
     <div className="flex flex-wrap gap-4">
          {images.map((image) => (
